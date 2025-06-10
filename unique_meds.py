2ed3c1583e943de161de23f126ed73627dfa01b0
MarsTL <165991919+MarsTL@users.noreply.github.com>
2025-06-09 17:42:54 -0700

    Doxorubicin:navitoclax  and Navitoclax and Doxorubicin to .csv

---
+++ unique_meds.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''separate drugs'''
+
+import pandas as pd
+
+df = pd.read_csv("leukemia_rnaseq_and_drug_response.csv")
+
+# Select only the Sample column and the three drug columns
+selected_cols = ["Sample", "Doxorubicin", "Navitoclax", "Doxorubicin:navitoclax"]
+subset_df = df[selected_cols]
+
+# Save to a new CSV
+subset_df.to_csv("unique_meds.csv", index=False)
+
+print("Saved drug columns to unique_meds.csv")
